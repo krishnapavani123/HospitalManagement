dfdc4692d2397e9c8f2ab2e938f25f2315316ff0
pavani <krishnapavani909@gmail.com>
2025-07-31 12:12:32 +0530

    updates2

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,10 +1,13 @@
 import  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../Shared/Navbar'
 import './index.css';
 
 class LandingPage extends Component {
   render() {
     return (
+      <div>
+        <Navbar/>
       <div className="landing-container">
         <div className="overlay">
           <h1 className="landing-title">Welcome to HealthCare Portal</h1>
@@ -15,6 +18,7 @@ class LandingPage extends Component {
             <Link to="/register/hospital" className="landing-btn">Register Hospital</Link>
           </div>
         </div>
+      </div>
       </div>
     );
   }
